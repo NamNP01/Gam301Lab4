9a8ddc7dcaec49d44f084e4812dd804036678b61
NamNP01 <namnppd10891@gmail.com>
2025-02-06 23:42:02 +0700

    first commit

---
+++ Assets/Script/Movement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Movement : MonoBehaviour
+{
+    public Transform Target; 
+    private NavMeshAgent agent;
+
+    public GameObject gameOverPanel; 
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        if (Target != null)
+        {
+            agent.SetDestination(Target.position);
+        }
+
+        if (!agent.pathPending && agent.remainingDistance <= agent.stoppingDistance)
+        {
+            GameOver();
+        }
+    }
+
+    void GameOver()
+    {
+        Debug.Log("Game Over! Quái đã đến đích!");
+        if (gameOverPanel != null)
+        {
+            gameOverPanel.SetActive(true); 
+        }
+        Time.timeScale = 0; 
+    }
+}
